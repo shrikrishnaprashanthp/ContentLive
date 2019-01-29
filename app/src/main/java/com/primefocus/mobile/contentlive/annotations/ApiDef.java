bf605a77a0ed2098549e789ab3bd6b64f525f104
Santosh Patar <santosh.patar@primefocus.com>
2019-01-29 18:02:56 +0530

    Add landing activity

--- app/src/main/java/com/primefocus/mobile/contentlive/annotations/ApiDef.java
+++ app/src/main/java/com/primefocus/mobile/contentlive/annotations/ApiDef.java
@@ -1,8 +1,19 @@
 package com.primefocus.mobile.contentlive.annotations;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class ApiDef {
 
+    @IntDef({CLApi.API_GET_TALENT })
+
+    @Retention(RetentionPolicy.SOURCE)
+
     public @interface CLApi {
+
+        int API_GET_TALENT = 0;
 
     }
 }
