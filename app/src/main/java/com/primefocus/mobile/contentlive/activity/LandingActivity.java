958734761375ef1d0f086dcb560cc85d2aeac50e
shrikrishna_p <shrikrishna.p@primefocus.com>
2019-01-30 11:40:26 +0530

    LoginFragment-FirstFragment-Added

--- app/src/main/java/com/primefocus/mobile/contentlive/activity/LandingActivity.java
+++ app/src/main/java/com/primefocus/mobile/contentlive/activity/LandingActivity.java
@@ -2,13 +2,19 @@ package com.primefocus.mobile.contentlive.activity;
 
 import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.primefocus.mobile.contentlive.R;
+import com.primefocus.mobile.contentlive.fragments.LoginFragment;
 
 public class LandingActivity extends AppCompatActivity {
 
+    FrameLayout loadingframelayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,13 @@ public class LandingActivity extends AppCompatActivity {
 
 
     void initializeAllComponents() {
+        loadingframelayout = findViewById(R.id.lyt_login_fragment_wrapper);
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment fraggy = new LoginFragment();
+        fragmentTransaction.add(R.id.lyt_login_fragment_wrapper, fraggy);
+        fragmentTransaction.commit();
     }
 }
